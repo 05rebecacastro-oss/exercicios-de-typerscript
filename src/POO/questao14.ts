48e61251b819b96e7b0f4afcfcef41a41c025118
josevitor211-byte <josevitor21122@gmail.com>
2026-09-10 13:30:14 -0300

    .

--- src/POO/questao14.ts
+++ src/POO/questao14.ts
@@ -3,3 +3,6 @@
 // publicação e disponibilidade (boolean). O programa deve permitir cadastrar até 15 livros via teclado,
 // listar todos os disponíveis e registrar o empréstimo de um livro pesquisado pelo título.
 
+export function executarQuestao14(): void {
+    
+}
